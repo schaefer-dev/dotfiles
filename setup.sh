0982e393c3d2b3596381560c54e338f2f0f1207a
Daniel Schäfer <schaefer.dev@gmail.com>
2015-06-30 18:47:20 +0200

    added sublime settings, also added airport syslink to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# create symbolic link to the 'airport' command:
+sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/sbin/airport
 
 # start systemlinking
 zsh ~/dotfiles/makesymlinks.sh
