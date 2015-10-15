0b682f041887df887e44c1b20322e7e24021cda7
Daniel Schäfer <schaefer.dev@gmail.com>
2015-10-15 22:24:18 +0200

    linking dotfiles in dotfiles.sh fixed

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -4,11 +4,66 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
+########## Main folder
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="tmux tmuxinator weechat"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
+
+########## Vim folder
+
+dir=~/dotfiles/mvim                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="vimrc vim gvimrc"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
+
+
+########## ZSH folder
+
+dir=~/dotfiles/zsh                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
