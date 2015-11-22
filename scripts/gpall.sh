a3aa23006b33174e0a30b2a96261e4acb34747fe
Daniel Schäfer <schaefer.dev@gmail.com>
2015-11-22 01:30:51 +0100

    gpall fixed

--- scripts/gpall.sh
+++ scripts/gpall.sh
@@ -5,13 +5,13 @@ CYANlight='\033[1;36m'
 NC='\033[0m' # No Color
 
 printf "${REDlight}"
-echo "pushing to origin ..."
-printf "${GREENlight}"
-git push -u origin master;
-printf "${REDlight}"
-echo "--------------------------------------------------------------"
 echo "pushing to my server ..."
 printf "${CYANlight}"
 git push -u server master;
+printf "${REDlight}"
+echo "--------------------------------------------------------------"
+echo "pushing to origin ..."
+printf "${GREENlight}"
+git push -u origin master;
 
 printf "${NC}"
