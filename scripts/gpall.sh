0fc64adfa97d44f630a8bf4695d7cdbeb2ea2c7f
Daniel Schäfer <schaefer.dev@gmail.com>
2015-12-14 22:50:59 +0100

    minor color changes in gpall

--- scripts/gpall.sh
+++ scripts/gpall.sh
@@ -4,12 +4,13 @@ GREENlight='\033[1;32m'
 CYANlight='\033[1;36m'
 NC='\033[0m' # No Color
 
-printf "${REDlight}"
-echo "pushing to my server ..."
 printf "${CYANlight}"
+echo "pushing to my server ..."
+printf "${GREENlight}"
 git push -u server master;
-printf "${REDlight}"
+printf "${NC}"
 echo "--------------------------------------------------------------"
+printf "${CYANlight}"
 echo "pushing to origin ..."
 printf "${GREENlight}"
 git push -u origin master;
