5d97ded170b2c1718e67d7efee7ed833506bc12d
Daniel Schäfer <schaefer.dev@gmail.com>
2016-05-20 13:36:56 +0200

    added basic config

---
+++ zsh/exports.zsh
@@ -0,0 +1,37 @@
+# Currently this path is appended to dynamically when picking a ruby version
+# zshenv has already started PATH with rbenv so append only here
+export PATH=$PATH~/bin:/usr/local/bin:/usr/local/sbin:~/bin
+
+# setting java_home
+export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+
+
+# Setup terminal, and turn on colors
+export TERM=xterm-256color
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+
+# Enable color in grep
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='3;33'
+
+# This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
+export ARCHFLAGS='-arch x86_64'
+
+export LESS='--ignore-case --raw-control-chars'
+export PAGER='less'
+export EDITOR='subl -w'
+
+#export NODE_PATH=/opt/github/homebrew/lib/node_modules
+#export PYTHONPATH=/usr/local/lib/python2.6/site-packages
+# CTAGS Sorting in VIM/Emacs is better behaved with this in place
+export LC_COLLATE=C 
+
+# important for lesspipe to work properly
+export LESSOPEN="|/usr/local/bin/lesspipe.sh %s"
+
+# use GNU tools as default
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+
