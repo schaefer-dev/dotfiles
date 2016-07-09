484f588d43db2ae6e9793cb2d5038e3969714c36
Daniel Schäfer <schaefer.dev@gmail.com>
2016-07-09 16:44:09 +0200

    added stopwatch and countdown functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -26,6 +26,22 @@ ex() {
     fi
 }
 
+
+countdown(){
+   date1=$((`date +%s` + $1)); 
+   while [ "$date1" -ge `date +%s` ]; do 
+     echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
+     sleep 0.1
+   done
+}
+stopwatch(){
+  date1=`date +%s`; 
+   while true; do 
+    echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r"; 
+    sleep 0.1
+   done
+}
+
 # -------------------------------------------------------------------
 # any function from http://onethingwell.org/post/14669173541/any
 # search for running processes
