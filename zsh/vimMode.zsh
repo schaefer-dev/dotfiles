7408017de543e2fcd3979c7468aa33b57ca72feb
Daniel Schäfer <schaefer.dev@gmail.com>
2016-08-02 04:13:43 +0200

    split vimMode into zsh script

---
+++ zsh/vimMode.zsh
@@ -0,0 +1,52 @@
+# enable vim mode
+bindkey -v
+export KEYTIMEOUT=1
+
+function print_dcs
+{
+  print -n -- "\EP$1;\E$2\E\\"
+}
+
+function set_cursor_shape
+{
+  if [ -n "$TMUX" ]; then
+    # tmux will only forward escape sequences to the terminal if surrounded by
+    # a DCS sequence
+    print_dcs tmux "\E]50;CursorShape=$1\C-G"
+  else
+    print -n -- "\E]50;CursorShape=$1\C-G"
+  fi
+}
+
+function zle-keymap-select zle-line-init
+{
+  # set cursor depending on mode
+  case $KEYMAP in
+    vicmd)
+      set_cursor_shape 0 # block cursor
+      ;;
+    viins|main)
+      set_cursor_shape 1 # line cursor
+      ;;
+  esac
+  
+  # show mode on the right side of terminal
+  RPS1="${${KEYMAP/vicmd/-- NORMAL --}/(main|viins)/-- INSERT --}"
+  RPS2=$RPS1
+
+  zle reset-prompt
+  zle -R
+}
+
+function zle-line-finish
+{
+  set_cursor_shape 0 # block cursor
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+
+# vim:set ft=zsh:
+
+bindkey -v
