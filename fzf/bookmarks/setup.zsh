7a43edfca39b64576528f36590a1870e2778d062
Daniel Schäfer <schaefer.dev@gmail.com>
2016-09-07 14:58:43 +0200

    implemented fzf bookmarks for folders and files

---
+++ fzf/bookmarks/setup.zsh
@@ -0,0 +1,10 @@
+rm -f /usr/local/bin/cdscuts_glob_echo
+rm -f /usr/local/bin/cdscuts_glob_echo_files
+rm -f /usr/local/bin/cdscuts_list_echo
+sudo chmod a+x ~/dotfiles/fzf/bookmarks/cdscuts_glob_echo
+sudo chmod a+x ~/dotfiles/fzf/bookmarks/cdscuts_glob_echo_files
+sudo chmod a+x ~/dotfiles/fzf/bookmarks/cdscuts_list_echo
+sudo ln -s ~/dotfiles/fzf/bookmarks/cdscuts_glob_echo /usr/local/bin/cdscuts_glob_echo
+sudo ln -s ~/dotfiles/fzf/bookmarks/cdscuts_glob_echo_files /usr/local/bin/cdscuts_glob_echo_files
+sudo ln -s ~/dotfiles/fzf/bookmarks/cdscuts_list_echo /usr/local/bin/cdscuts_list_echo
+
