eaddb3d663a361b37becf6527dffed46eed87e87
Daniel Schäfer <schaefer.dev@gmail.com>
2016-09-07 14:42:48 +0200

    implemented basic fzf into zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -299,14 +299,3 @@ function web_search() {
   open "$url"
 }
 
-
-function fzvim() {
-  local out file key
-  out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
-  key=$(head -1 <<< "$out")
-  file=$(head -2 <<< "$out" | tail -1)
-  if [ -n "$file" ]; then
-    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
-  fi
-}
-
