6d01d8e51411a0589d75424b89a10efae6fe0d01
Daniel Schäfer <schaefer.dev@gmail.com>
2017-02-23 14:52:29 +0100

    zsh and tmux simple green theme

--- zsh/promptOneLine.zsh
+++ zsh/promptOneLine.zsh
@@ -1,7 +1,7 @@
 # Loading a Prompt in one Line Format
 
 PROMPT='
-%{$reset_color%}${PR_RED}$(get_pwd) $(prompt_char) %{$reset_color%}'
+%{$reset_color%}${PR_BOLD_GREEN}$(get_pwd) ${PR_BOLD_BLACK}$(prompt_char) %{$reset_color%}'
 
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
 
