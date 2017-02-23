234e546e008fc30f2ef4b4ad77d3e8171bbe14d4
Daniel Schäfer <schaefer.dev@gmail.com>
2017-02-23 20:50:17 +0100

    livestream and web function moved to scripts

---
+++ scripts/web.zsh
@@ -0,0 +1,140 @@
+# Web function to open Websites
+web(){
+	if [ "$1" = "reddit" ]; then
+		if [ "$2" = "headphones" ]; then
+			open 'http://www.reddit.com/r/headphones/'
+			return
+		fi
+		if [ "$2" = "headphones" ]; then
+			open 'http://www.reddit.com/r/headphones/'
+			return
+		fi
+		if [ "$2" = "lol" ]; then
+			open 'http://www.reddit.com/r/leagueoflegends/'
+			return
+		fi
+		if [ "$2" = "csgo" ]; then
+			open 'http://www.reddit.com/r/globaloffensive/'
+			return
+		fi
+		if [ "$2" = "mechanicalkeyboards" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		if [ "$2" = "mechs" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		if [ "$2" = "keyboard" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		if [ "$2" = "keyboards" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		if [ "$2" = "hearthstone" ]; then
+			open 'http://www.reddit.com/r/hearthstone/'
+			return
+		fi
+		if [ "$2" = "hs" ]; then
+			open 'http://www.reddit.com/r/hearthstone/'
+			return
+		fi
+		open 'http://www.reddit.com/r/Dashboard/'
+		return
+	fi
+
+	if [ "$1" = "youtube" ] || [ "$1" = "yt" ]; then
+		if [ "$2" = "watchlater" ]; then
+			open 'https://www.youtube.com/playlist?list=WL'
+			return
+		fi
+		if [ "$2" = "wl" ]; then
+			open 'https://www.youtube.com/playlist?list=WL'
+			return
+		fi
+		open 'https://www.youtube.com/?gl=DE&hl=de'
+		return
+	fi
+
+
+	if [ "$1" = "hlcv" ] || [ "$1" = "highlevel" ]; then
+        open 'http://www.mpi-inf.mpg.de/departments/computer-vision-and-multimodal-computing/teaching/courses/high-level-computer-vision/'
+	fi
+
+	if [ "$1" = "hci" ] || [ "$1" = "hcidev" ]; then
+        open 'http://umtl.cs.uni-saarland.de/index.php?id=288'
+	fi
+
+	if [ "$1" = "sec" ] || [ "$1" = "security" ]; then
+        if [ "$2" = "piazza" ]; then
+            open "https://piazza.com/class/in00qm5p95r5oh"
+            return
+        fi
+        if [ "$2" = "material" ]; then
+            open "https://piazza.com/uni-saarland.de/spring2016/cs81908/resources"
+            return
+        fi
+        if [ "$2" = "resources" ]; then
+            open "https://piazza.com/uni-saarland.de/spring2016/cs81908/resources"
+            return
+        fi
+        if [ "$2" = "ladmin" ]; then
+            open "https://www.sps.cs.uni-saarland.de/teaching/16SS/security/ladmin/index.php"
+            return
+        fi  
+		open 'https://www.sps.cs.uni-saarland.de/teaching/16SS/security/index.html'
+		return
+	fi
+
+    if [ "$1" = "netflix" ]; then
+		open 'http://www.netflix.com'
+		return
+	fi
+
+	if [ "$1" = "cal" ] || [ "$1" = "calendar" ]; then
+		open 'https://calendar.google.com/calendar/b/0/render#main_7'
+		return
+	fi
+
+	if [ "$1" = "gh" ] || [ "$1" = "github" ]; then
+		open 'https://github.com/schaefer-dev'
+		return
+	fi
+
+    if [ "$1" = "twitch" ]; then
+		open 'http://www.twitch.tv'
+		return
+	fi
+
+    if [ "$1" = "gg" ]; then
+		open 'https://dejure.org/gesetze/GG'
+		return
+	fi
+
+    if [ "$1" = "stgb" ]; then
+		open 'https://dejure.org/gesetze/StGB'
+		return
+	fi
+    if [ "$1" = "recht" ] || [ "$1" = "jura" ]; then
+        if [ "$2" = "material" ]; then
+            open "https://hyperion.cispa.saarland/folien/2016-WS/Recht-Cybersicherheit/"
+            return
+        fi
+		open 'http://www.uni-saarland.de/lehrstuhl/sorge/lehre/winter-20162017/vorlesung-recht-der-cybersicherheit.html'
+		return
+	fi
+    if [ "$1" = "pec" ] || [ "$1" = "pet" ]; then
+        if [ "$2" = "piazza" ]; then
+            open "https://piazza.com/uni-saarland.de/winter2017/cs17/home"
+            return
+        fi
+		open 'http://www.sps.cs.uni-saarland.de/teaching/16WS/pec/index.html'
+		return
+	fi
+
+	echo "invalid input after function call"
+}
+
+
