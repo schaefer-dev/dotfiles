35a644923987246f4cec43c938639d213ab3ed6f
Daniel Schäfer <schaefer.dev@gmail.com>
2017-11-05 03:54:56 +0100

    tab stop sized 2 in emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -127,6 +127,10 @@
 (setq mac-command-modifier 'none)
 (setq ns-function-modifier 'hyper)
 
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+
+(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
 
 
 ;; OTHER CUSTOMIZATION
