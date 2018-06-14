7c80f98de197f05fd2b2d44ee9e2c39888adf9fd
Daniel Schaefer <schaefer.dev@gmail.com>
2018-06-14 17:22:00 +0200

    minimal prompt update

--- zsh/promptMinimal.zsh
+++ zsh/promptMinimal.zsh
@@ -5,4 +5,4 @@ PROMPT='
 
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
 
-RPROMPT='%{$reset_color%}${PR_BOLD_MAGENTA}$(get_pwd)%{$reset_color%} $fg[white]$(git_prompt_string)${PR_BOLD_GREEN}'
+RPROMPT='%{$reset_color%}${PR_BOLD_MAGENTA}$(get_pwd)%{$reset_color%}$fg[white] $(git_prompt_string)${PR_BOLD_GREEN}'
