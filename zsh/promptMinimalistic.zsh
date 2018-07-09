c602cad700d8bedf827aefa7b855a90af17d3909
Daniel Schaefer <schaefer.dev@gmail.com>
2018-07-09 17:26:24 +0200

    branch information rearranged

--- zsh/promptMinimalistic.zsh
+++ zsh/promptMinimalistic.zsh
@@ -7,4 +7,4 @@ zle_highlight=( default:bold,fg=green )
 
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
 
-RPROMPT='${PR_BOLD_MAGENTA}$(get_pwd) ${PR_WHITE}$(git_prompt_string)${PR_BOLD_GREEN}'
+RPROMPT='${PR_WHITE}$(git_prompt_string) ${PR_BOLD_MAGENTA}$(get_pwd)${PR_BOLD_GREEN}'
