2e5f29032a9a8c4bd4a514fb4f0d473e9990622b
Daniel Schaefer <schaefer.dev@gmail.com>
2018-10-27 19:58:40 +0200

    added setting to disable mouse acceleration

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,3 +9,6 @@ zsh ~/dotfiles/setup/brew.sh
 
 # install livestreamer tool https://github.com/chrippa/livestreamer
 pip install livestreamer
+
+# disable mouse acceleration
+defaults write -g com.apple.mouse.scaling -1
