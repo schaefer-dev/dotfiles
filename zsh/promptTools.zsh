5cd9a62317a9c51b44813e9e61642244a0fc0fa9
Daniel Schäfer <schaefer.dev@gmail.com>
2018-11-13 15:01:03 +0100

    switched back to zsh with updated stuff

--- zsh/promptTools.zsh
+++ zsh/promptTools.zsh
@@ -4,9 +4,9 @@ function prompt_char {
     #git branch >/dev/null 2>/dev/null && echo '❯' && return
     #hg root >/dev/null 2>/dev/null && echo '❯' && return
     #echo '❯'
-    git branch >/dev/null 2>/dev/null && echo '' && return
-    hg root >/dev/null 2>/dev/null && echo '' && return
-    echo ''
+    git branch >/dev/null 2>/dev/null && echo '>' && return
+    hg root >/dev/null 2>/dev/null && echo '>' && return
+    echo '>'
 }
 
 function box_name {
