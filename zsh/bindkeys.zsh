5980b9667e114f89f133b9d23ef10aa3aeba8a58
Daniel Schäfer <schaefer.dev@gmail.com>
2019-02-01 11:09:56 +0100

    disabled vim mode in zsh

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -2,17 +2,13 @@
 # cat > /dev/null
 # And press it
 
+bindkey -e   # Default to standard vi bindings, regardless of editor string
 # bindkey "^K"      kill-whole-line                      # ctrl-k
 # bindkey "^R"      history-incremental-search-backward  # ctrl-r
-# bindkey "^A"      beginning-of-line                    # ctrl-a  
+# bindkey "^I"      beginning-of-line                    # ctrl-a  
 # bindkey "^E"      end-of-line                          # ctrl-e
 # bindkey "[B"      history-search-forward               # down arrow
 # bindkey "[A"      history-search-backward              # up arrow
 # bindkey "^D"      delete-char                          # ctrl-d
 # bindkey "^F"      forward-char                         # ctrl-f
 # bindkey "^B"      backward-char                        # ctrl-b
-# bindkey -v   # Default to standard vi bindings, regardless of editor string
-
-
-# backwards completion
-bindkey '^[[Z' reverse-menu-complete
