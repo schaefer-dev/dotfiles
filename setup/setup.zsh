9180d36e0f7a566d3eb6f11f7396e2ca5a448219
Daniel Schäfer <schaefer.dev@gmail.com>
2020-03-29 19:23:20 +0200

    color updates to iTerm and animation speedups

--- setup/setup.zsh
+++ setup/setup.zsh
@@ -9,3 +9,13 @@ pip install livestreamer
 
 # disable mouse acceleration
 defaults write -g com.apple.mouse.scaling -1
+
+# disable smooth scrolling
+# defaults write -g NSScrollAnimationEnabled -bool false
+
+# disable animation for opening and closing windows and popovers
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool NO
+
+# fast dialog boxes appearing (default is .2)
+defaults write NSGlobalDomain NSWindowResizeTime .05
